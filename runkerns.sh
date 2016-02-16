c0f9a217cf7b4fe424373eeae65b671450ce137d
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2016-02-16 10:32:35 +1000

    updated slurm script options and name

--- runkerns.sh
+++ runkerns.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=dvs
-#SBATCH --output=slurm-job.out
-#SBATCH --error=slurm.err
 #SBATCH --partition=batch
 #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mail-type=end
 #SBATCH --mail-user=joshua.arnold1@uqconnect.edu.au
 
-srun matlab -nodisplay -r 'kernComGrow, exit'
+srun --gres=gpu:1 -n1 --exclusive matlab -nodisplay -r 'gpuEvolveKerns, exit'
 
 wait
